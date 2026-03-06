bdcb1de0b047ae750cbe91c2fb6a3e0587c11ef2
richlifeguideonly-prog <richlifeguideonly@gmail.com>
2026-03-06 07:52:19 +0100

    add core index exports

---
+++ packages/core/index.ts
@@ -0,0 +1,9 @@
+// packages/core/index.ts
+
+export * from "./constants";
+export * from "./fee";
+
+export * from "./swap-engine/state";
+export * from "./swap-engine/validate";
+
+export * from "./nostr/offers";
